bb6985a48038b5474749d0894f74e37355f5fcab
Kalana Maduranga <kalanamadu2000@gmail.com>
2026-01-15 05:19:06 +0530

    authService completed

--- services/authService.ts
+++ services/authService.ts
@@ -1,33 +1,43 @@
-import { auth , db } from "./firebaseConfig"
+import { auth, db } from "./firebaseConfig"
 import {
-    signInWithEmailAndPassword,
-    createUserWithEmailAndPassword,
-    updateProfile,
-    signOut,
-    User
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  updateProfile,
+  signOut,
+  User
 } from "firebase/auth"
-import { doc ,setDoc , updateDoc } from "firebase/firestore"
+import { doc, setDoc, updateDoc } from "firebase/firestore"
 
-export const loginUser = async (email: string , password: string) => {
-    return signInWithEmailAndPassword(auth , email , password)
+export const loginUser = async (email: string, password: string) => {
+  return signInWithEmailAndPassword(auth, email, password)
 }
 
 export const registerUser = async (
+  fullName: string,
+  email: string,
+  password: string
+) => {
+  const userCredential = await createUserWithEmailAndPassword(
+    auth,
+    email,
+    password
+  )
+  await updateProfile(userCredential.user, { displayName: fullName })
+  await setDoc(doc(db, "users", userCredential.user.uid), {
+    fullName,
+    email,
+    createdAt: new Date()
+  })
+  return userCredential.user
+}
 
-    fullName: string,
-    email: string,
-    password: string
+export const logoutUser = async () => {
+  return signOut(auth)
+}
 
-)=>{
-    const userCredential = await createUserWithEmailAndPassword(
-        auth,
-        email,
-        password
-    )
-    await updateProfile(userCredential.user , { displayName: fullName})
-    await setDoc(doc(db, "user" , userCredential.user.uid), {
-        fullName,
-        email,
-        createdAt: new Date()
-    })
+export const updateUserProfile = async (user: User, displayName: string) => {
+  await updateProfile(user, { displayName })
+
+  const userRef = doc(db, "users", user.uid)
+  await updateDoc(userRef, { fullName: displayName })
 }
