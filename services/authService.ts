efb382b75c5a4cd092473b24ee9317f5969c7bc9
Kalana Maduranga <kalanamadu2000@gmail.com>
2026-01-15 05:11:45 +0530

    authService update

--- services/authService.ts
+++ services/authService.ts
@@ -12,3 +12,15 @@ export const loginUser = async (email: string , password: string) => {
     return signInWithEmailAndPassword(auth , email , password)
 }
 
+export const registerUser = async (
+    fullName: string,
+    email: string,
+    password: string
+)=>{
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      )
+
+}
