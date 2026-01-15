143e3be5c35ebd0656ab305a64a53d62c2c980a4
Kalana Maduranga <kalanamadu2000@gmail.com>
2026-01-15 14:38:33 +0530

    layout , index , register files updated

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,23 @@
-import { Stack } from "expo-router";
-import { AuthProvider } from "../context/AuthContext";
+import React from 'react'
+import { StatusBar } from 'expo-status-bar'
+import { Slot } from 'expo-router'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import Toast from 'react-native-toast-message'
+import '../globals.css'
+import { AuthProvider } from '@/context/AuthContext'
+import { LoaderProvider } from '@/context/LoaderContext'
 
-export default function RootLayout() {
+export default function RootLayout () {
   return (
-    <AuthProvider>
-      <Stack screenOptions={{ headerShown: false }} />
-    </AuthProvider>
-  );
+    <LoaderProvider>
+      <AuthProvider>
+        <SafeAreaView className='flex-1 bg-gray-50'>
+          <StatusBar style='dark' />
+          <Slot />
+        </SafeAreaView>
+        <Toast />
+      </AuthProvider>
+    </LoaderProvider>
+  )
 }
+  
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,21 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import "../global.css"
+import { useAuth } from '@/hooks/useAuth'
+import { Redirect } from 'expo-router'
+import { View, ActivityIndicator } from 'react-native'
 
-const index = () => {
-  return (
-    <View>
-      <Text>index</Text>
-    </View>
-  )
+export default function Index () {
+  const { user, loading } = useAuth()
+
+  if (loading) {
+    return (
+      <View className='flex-1 justify-center items-center bg-gray-50'>
+        <ActivityIndicator size='large' color='#4ade80' />
+      </View>
+    )
+  }
+
+  if (user) {
+    return <Redirect href='./(dashboard)/home' />
+  } else {
+    return <Redirect href='./(auth)/login' />
+  }
 }
-
-export default index
-
-const styles = StyleSheet.create({})
