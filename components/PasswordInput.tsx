9b0ba824dbe938f5e327caa66258ffc8d702a711
Kalana Maduranga <kalanamadu2000@gmail.com>
2026-01-15 14:31:17 +0530

    Password input file created

---
+++ components/PasswordInput.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import { View , TextInput , TouchableOpacity } from "react-native"
+import { Ionicons } from '@expo/vector-icons'
+
+export default function PasswordInput({
+    
+    password,
+    setPassword,
+    placeholder
+    {:{
+
+        
+    }}
+})
