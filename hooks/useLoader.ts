eaf490213c7101f26bbe443ad0bd51e63cbea84f
Kalana Maduranga <kalanamadu2000@gmail.com>
2026-01-15 14:11:08 +0530

    useLoader file created

--- hooks/useLoader.ts
+++ hooks/useLoader.ts
@@ -1,1 +1,10 @@
-import { LoaderContext } "@con"
+import { LoaderContext } from "@/context/LoaderContext"
+import { useContext } from "react"
+
+export const useLoader = () => {
+//   const context = useContext(LoaderContext)
+//   if (!context) {
+//     throw new Error("useLoader must be used within a LoaderProvider")
+//   }
+//   return context
+}
